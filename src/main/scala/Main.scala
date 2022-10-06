06174d50cd47f500dcdf46821579aaea0b5c57a8
cai-i <94360420+cai-i@users.noreply.github.com>
2022-10-06 19:22:23 +0200

    Main : communication between client and server

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,33 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-def msg = "I was compiled by Scala 3. :)"
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+object ServerMain {
+
+  def main(args: Array[String]): Unit = {
+    val server = new ServerSocket(8182)
+
+    try
+      while (true) {
+        val client = server.accept()
+
+        try {
+          val source = Source.fromInputStream(client.getInputStream)
+          for (elem <- source.getLines().takeWhile(_.trim.nonEmpty))
+            println(elem)
+          println("")
+
+          val printer = new PrintWriter(client.getOutputStream)
+          printer.print("HTTP/1.1 200 OK\r\n")
+          printer.print("Date: Tue, 6 Sep 2022 14:01:07 +0200\r\n")
+          printer.print("Content-Type: application/json\r\n")
+          printer.print("\r\n")
+          printer.print("""{"response": "hello"}""")
+          printer.flush()
+        } finally client.close()
+      }
+    finally server.close()
+  }
+
+}
